3936d01f068bcd09a5d4be18f0b55496476d67a3
Selim Erunkut <se@novemgold.com>
2020-10-27 17:35:53 +0100

    latest changes

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,11 @@
-const NNN = artifacts.require("NNN Token");
-const Roles = artifacts.require("Roles");
-const WhitelistAdminRole = artifacts.require("WhitelistAdminRole");
-const WhitelistedRole = artifacts.require("WhitelistedRole");
+const NNN = artifacts.require("NNNToken");
+//const Roles = artifacts.require("Roles");
+//const WhitelistAdminRole = artifacts.require("WhitelistAdminRole");
+//const WhitelistedRole = artifacts.require("WhitelistedRole");
 
 module.exports = function(deployer) {
-  deployer.deploy(NNN);
-  //deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(Roles);
-  deployer.deploy(WhitelistAdminRole);
-  deployer.deploy(WhitelistedRole);
+  deployer.deploy(NNN, 200, "0x23d14f765b8B8B0Ca604fF673E84cD6BB286021f");
+  //deployer.deploy(Roles);
+  //deployer.deploy(WhitelistAdminRole);
+  //deployer.deploy(WhitelistedRole);
 };
